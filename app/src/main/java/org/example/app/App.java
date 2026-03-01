645c15ebafb37e3a90e8e4c31b012cd6887eb8b2
Som-Kesharwani <developer.kesharwanisom@gmail.com>
2026-03-01 18:38:38 +0530

    project setup

--- app/src/main/java/org/example/app/App.java
+++ app/src/main/java/org/example/app/App.java
@@ -3,19 +3,18 @@
  */
 package org.example.app;
 
-import org.example.list.LinkedList;
-
+import static org.example.app.MessageUtils.getMessage;
 import static org.example.utilities.StringUtils.join;
 import static org.example.utilities.StringUtils.split;
-import static org.example.app.MessageUtils.getMessage;
 
 import org.apache.commons.text.WordUtils;
+import org.example.list.LinkedList;
 
 public class App {
-    public static void main(String[] args) {
-        LinkedList tokens;
-        tokens = split(getMessage());
-        String result = join(tokens);
-        System.out.println(WordUtils.capitalize(result));
-    }
+  public static void main(String[] args) {
+    LinkedList tokens;
+    tokens = split(getMessage());
+    String result = join(tokens);
+    System.out.println(WordUtils.capitalize(result));
+  }
 }
